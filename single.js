263c8804d00a6194dd2986e4596a721c0ab873b1
Sargsyan4444 <ruzan.sargsyan.444@gmail.com>
2025-04-04 16:49:06 +0400

    videos commit

--- single.js
+++ single.js
@@ -3,6 +3,7 @@ let movieId = location.href.split('=')[1];
 let bgcImage = document.getElementById('movie__container');
 let smallImg = document.getElementById('smallImg');
 let actorsList = document.getElementById('actorsList');
+let movieBox = document.getElementById('movieBox')
 
 let api_key = "api_key=9b702a6b89b0278738dab62417267c49";
 let img_url_original = "https://image.tmdb.org/t/p/original";
@@ -35,4 +36,18 @@ fetch(`https://api.themoviedb.org/3/movie/${movieId}?${api_key}`)
             <h3>${actor.name}</h3>
         </div>
         `
+    }))
+
+
+    fetch(`https:api.themoviedb.org/3/movie/${movieId}/videos?${api_key}`)
+    .then(res=>res.json())
+    .then(res => res.results.forEach((e)=>{
+        let videoBox = document.createElement('div')
+        videoBox.classList.add('videoBox')
+        videoBox.innerHTML=`
+            <iframe width="150" height="150" src="https://www.youtube.com/embed/${e.key}"
+             title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
+            gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        `
+        movieBox.append(videoBox)
     }))
