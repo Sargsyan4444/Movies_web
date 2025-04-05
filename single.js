f7eaddc3eb72f7e345e95666e69dbeb70658a4c0
Sargsyan4444 <ruzan.sargsyan.444@gmail.com>
2025-04-05 14:40:02 +0300

    i added videos but without popup

--- single.js
+++ single.js
@@ -4,6 +4,9 @@ let bgcImage = document.getElementById('movie__container');
 let smallImg = document.getElementById('smallImg');
 let actorsList = document.getElementById('actorsList');
 let movieBox = document.getElementById('movieBox')
+let popupOverlay = document.getElementById('popupOverlay');
+let popupIframe = document.getElementById('popupIframe');
+let closePopup = document.getElementById('closePopup');
 
 let api_key = "api_key=9b702a6b89b0278738dab62417267c49";
 let img_url_original = "https://image.tmdb.org/t/p/original";
@@ -16,14 +19,16 @@ fetch(`https://api.themoviedb.org/3/movie/${movieId}?${api_key}`)
         smallImg.src = img_url + res.poster_path;
         let genres = res.genres.map(genre => genre.name).join(', ');
         movieInfo.innerHTML = `
+        <div class="mainInfo">
         <h1>${res.title}</h1>
+        <p>${res.overview}</p>
+        </div>
         <div class="littleInfo">
         <h2>Genre: ${genres}</h2>
         <h2>Language: ${res.original_language}</h2>
         <h3>${res.status}</h3>
         <h2>Release Date: ${res.release_date}</h2>
         </div>
-            <p>${res.overview}</p>
         `;
     })
 
@@ -41,15 +46,22 @@ fetch(`https://api.themoviedb.org/3/movie/${movieId}?${api_key}`)
     }))
 
 
-    // fetch(`https:api.themoviedb.org/3/movie/${movieId}/videos?${api_key}`)
-    // .then(res=>res.json())
-    // .then(res => res.results.forEach((e)=>{
-    //     let videoBox = document.createElement('div')
-    //     videoBox.classList.add('videoBox')
-    //     videoBox.innerHTML=`
-    //         <iframe width="400" height="400" src="https://www.youtube.com/embed/${e.key}"
-    //          title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
-    //         gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-    //     `
-    //     movieBox.append(videoBox)
-    // }))
+    fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos?${api_key}`)
+    .then(res => res.json())
+    .then(res => {
+      if (res.results.length === 0) {
+        movieBox.innerHTML = `<p style="color: #E50000; font-weight: bold; font-size: 18px;">No trailers found</p>`;
+      } else {
+        res.results.forEach((e) => {
+          let videoBox = document.createElement('div');
+          videoBox.classList.add('videoBox');
+          videoBox.innerHTML = `
+            <iframe width="400" height="400" src="https://www.youtube.com/embed/${e.key}"
+            title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write;
+            encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          `;
+          movieBox.append(videoBox);
+        });
+      }
+    });
